fe9271780b01528aee86469a528b084232557d76
Rus <5333255@gmail.com>
2024-11-17 16:41:06 +0300

    rename + new task

---
+++ main.py
@@ -0,0 +1,8 @@
+my_set={1,1,2,3,5,4,5,4,8,'yes','yes',1,2}
+print('set:',my_set)
+my_set.add(7)
+my_set.add('no')
+print('Modified set:',my_set, '+"7" and "no"')
+my_set.discard(2)
+print('Modified set:',my_set,'- "2"')
+
