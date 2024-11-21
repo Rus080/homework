e4edb983daf7550280008accc531d4a416a69e90
Rus <5333255@gmail.com>
2024-11-21 22:24:34 +0300

    new task

--- module_1_5.py
+++ module_1_5.py
@@ -1,8 +1,8 @@
 immutable_var = 1, 2, 'yes', True
 print('immutable_var = ', immutable_var)
 # immutable_var[0] = 8
-print(
-    '"immutable_var[0] = 8" - Изменение внести не возможно, так как данные внесенные в кортеж является постоянными,за исключением случаев когда в кортеж внесен список')
+print('''"immutable_var[0] = 8" - Изменение внести не возможно, 
+так как данные внесенные в кортеж является постоянными,за исключением случаев когда в кортеж внесен список''')
 
 mutable_list = ['apple', 1, 2, True]
 print('mutable_list before = ', mutable_list)
