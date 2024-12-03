83a3c4808052de9d604b6ff38d252bacc5f89792
Rus <5333255@gmail.com>
2024-12-03 21:14:56 +0300

    correct module_2_6

--- module_2_6.py
+++ module_2_6.py
@@ -1,15 +1,24 @@
+from multiprocessing.managers import Value
+
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 result = []
 
-while True:  #Ввод первой переменной
-    n = int(input('Введите число для первой вставки: '))
+def get_n():
+    while True:  #Ввод первой переменной
+        try:
+            n = int(input('Введите число для первой вставки: '))
+            return n
+        except ValueError:
+            print('Вы ввели не число, повторите ввод.')
+n = get_n()
+while True:
     if n not in range(3,21):
         print("Повторите ввод числа в интервале от 3 до 20")
+        n = get_n()
         continue
     else:
         print('Число из первой вставки =', n)
         break
-
 for i in range(1, len(numbers)):
     for j in range(i, len(numbers)):
         if (i + j) % n == 0 or n % (i + j) == 0:
