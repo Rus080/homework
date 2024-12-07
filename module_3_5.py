dc990b4565dc750f65054343581068a1b584af7c
Rus <5333255@gmail.com>
2024-12-07 23:25:53 +0300

    new task

---
+++ module_3_5.py
@@ -0,0 +1,18 @@
+#Задача "Рекурсивное умножение цифр"
+
+def get_multiplied_digits (number):
+    str_number = str(number)
+    if int(str_number[0]) == 0:
+        first = 1
+    else:
+        first = int(str_number[0])
+    if len(str_number) > 1:
+        if first != 0:
+            return first * get_multiplied_digits(int(str_number[1:]))
+    else:
+        return first
+
+result = get_multiplied_digits(40203)
+print(result)
+result2 = get_multiplied_digits(402030)
+print(result2)
