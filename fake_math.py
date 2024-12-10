0c46245be48a31906aecd1ebf19ef4fb3c79fe18
Rus <5333255@gmail.com>
2024-12-10 23:19:00 +0300

    new task

---
+++ fake_math.py
@@ -0,0 +1,9 @@
+# Дополнение к модулю 4_1
+
+
+def fake_divide (first, second) :
+    if second == 0:
+        result = 'Error!'
+    else:
+        result = first / second
+    return result
---
+++ true_math.py
@@ -0,0 +1,10 @@
+# Дополнение к модулю 4_1
+from math import inf
+
+
+def true_divide (first, second) :
+    if second == 0:
+        return inf
+    else:
+        result = first / second
+    return result
