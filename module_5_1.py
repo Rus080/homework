56baaa0818177cf46cd259ec23cd85b8595bbb2b
Rus <5333255@gmail.com>
2024-12-24 23:48:05 +0300

    new task

--- module_5_1.py
+++ module_5_1.py
@@ -1,12 +1,10 @@
 # Задача "Developer - не только разработчик"
 
 
-
 class House:
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
-
 
     def go_to (self, new_floor):
         print(f'{self.name}, количество этажей в доме: {self.number_of_floors}')
@@ -29,6 +27,35 @@ class House:
         #return self.number_of_floors
 
 
+# Задача "Нужно больше этажей"
+    def __eq__(self, other):
+        return self.number_of_floors == other
+
+    def  __lt__(self, other):
+        return self.number_of_floors < other
+
+    def __le__(self, other):
+        return self.number_of_floors <= other
+
+    def  __gt__(self, other):
+        return self.number_of_floors > other
+
+    def  __ge__(self, other):
+        return self.number_of_floors >= other
+
+    def  __ne__(self, other):
+        return self.number_of_floors != other
+
+    def __add__(self, other):
+        return House(self.name, self.number_of_floors + other)
+
+    def __radd__(self, other):
+        return House(self.name, self.number_of_floors + other)
+
+    def __iadd__(self, other):
+        return House(self.name, self.number_of_floors + other)
+
+
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
 #h1.go_to(6)
@@ -41,3 +68,19 @@ print(h2)
 # __len__
 print(len(h1))
 print(len(h2))
+
+print(h1 == h2) # __eq__
+h1 = h1 + 10 # __add__
+print(h1)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 36 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
